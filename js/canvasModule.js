367f3563ea5571f64493af55e097ada4216893da
Cailin Li <cxl2467@rit.edu>
2015-12-02 15:04:15 -0500

    Draw connections between wine food pairings

--- js/canvasModule.js
+++ js/canvasModule.js
@@ -8,6 +8,60 @@ var app = app || {};
  */
 app.canvasModule = (function() {
     var animationId = 0,
+        WINE_FOOD_MAP = {
+            '#sparkling': [
+                '#fish', 
+                '#bread', 
+                '#hardcheese', 
+                '#cheese', 
+                '#cabbage'
+            ],
+            '#drywhite': [
+                '#cabbage',
+                '#kebab',
+                '#fish'
+            ],
+            '#sweetwhite': [
+                '#cheese',
+                '#hardcheese',
+                '#salami',
+                '#cupcake'
+            ],
+            '#richwhite': [
+                '#cheese',
+                '#bread',
+                '#fish',
+                '#crab',
+                '#chicken'
+            ],
+            '#dessertwine': [
+                '#cupcake',
+                '#bread',
+                '#salami'
+            ],
+            '#lightred': [
+                '#crab',
+                '#bread',
+                '#chicken',
+                '#kebab'
+            ],
+            '#mediumred': [
+                '#kebab',
+                '#cheese',
+                '#bread',
+                '#chicken',
+                '#steak',
+                '#salami'
+            ],
+            '#boldred': [
+                '#bread',
+                '#hardcheese',
+                '#steak',
+                '#salami'
+            ],
+        },
+        TOP_OFFSET = 15,
+        BOTTOM_OFFSET = 10,
         canvas, ctx;
 
     function init() {
@@ -17,11 +71,42 @@ app.canvasModule = (function() {
         update();
     }
 
+    function draw() {
+        $.each(WINE_FOOD_MAP, function(wineID, foods) {
+            $.each(foods, function(index, foodID) {
+                var x1 = calculateLeft(wineID) - 12;
+                var y1 = canvas.height - BOTTOM_OFFSET;
+                var x2 = calculateLeft(foodID) - 15;
+                var y2 = TOP_OFFSET;
+
+                drawConnection(x1, y1, x2, y2);                
+            });
+        });
+
+        // ctx.beginPath();
+        // ctx.arc(left, canvas.height - 10, 1, 0, Math.PI * 2, false);
+        // ctx.closePath();
+    }
+
+    function drawConnection(x1, y1, x2, y2) {
+        ctx.beginPath();
+        ctx.moveTo(x1, y1);
+        ctx.lineTo(x2, y2);
+        ctx.closePath();
+
+        ctx.strokeStyle = 'mediumSeaGreen';
+        ctx.stroke();
+    }
+
     function update() {
         animationId = requestAnimationFrame(update.bind(this));
 
-        console.log($(canvas).parent().height());
         canvas.height = $(canvas).parent().height();
+        draw();
+    }
+
+    function calculateLeft(id) {
+        return $(id).position().left + ($(id).width() / 2);
     }
 
     return {
